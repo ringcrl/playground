6ae0fb79db6ced8d57a792e08522ed8f1078faf4
chenng <ringcrl@foxmail.com>
2021-07-23 11:33:46 +0800

    feat: update

--- 21-07-22-spritesmith/main.js
+++ 21-07-22-spritesmith/main.js
@@ -3,15 +3,13 @@ const Spritesmith = require('spritesmith');
 const fs = require('fs');
 const path = require('path');
 
-const cacheDir = path.resolve(__dirname, './cache');
-let sprites = fs.readdirSync(path.resolve(__dirname, './cache'));
-sprites = sprites.map((spriteName) => path.resolve(cacheDir, spriteName));
+const spriteDir = path.resolve(__dirname, './cache');
+let sprites = fs.readdirSync(spriteDir);
+sprites = sprites.map((spriteName) => path.resolve(spriteDir, spriteName));
 
-// Generate our spritesheet
-// const sprites = ['fork.png', 'github.png', 'twitter.png'];
 Spritesmith.run({ src: sprites, algorithm: 'top-down' }, (err, result) => {
-  result.image; // Buffer representation of image
-  result.coordinates; // Object mapping filename to {x, y, width, height} of image
-  result.properties; // Object with metadata about spritesheet {width, height}
+  // result.image; // Buffer representation of image
+  // result.coordinates; // Object mapping filename to {x, y, width, height} of image
+  // result.properties; // Object with metadata about spritesheet {width, height}
   fs.writeFileSync(`${__dirname}/sprite.png`, result.image);
 });
