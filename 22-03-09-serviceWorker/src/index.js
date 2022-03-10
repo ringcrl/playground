1c2a648d5ca8ed004ead5374cba2ff21b6d0bcac
chenng <ringcrl@foxmail.com>
2022-03-10 12:01:20 +0800

    feat: update

--- 22-03-09-serviceWorker/src/index.js
+++ 22-03-09-serviceWorker/src/index.js
@@ -1,13 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js').then((registration) => {
-      console.log('SW registered: ', registration);
-    }).catch((registrationError) => {
-      console.log('SW registration failed: ', registrationError);
-    });
-  });
-}
-
 function component() {
   const element = document.createElement('div');
 
