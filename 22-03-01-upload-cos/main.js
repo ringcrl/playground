026cacc66d603d8bfd18de6b6382c6db9de2cf4c
chenng <ringcrl@foxmail.com>
2022-03-22 17:51:11 +0800

    feat: update

--- 22-03-01-upload-cos/main.js
+++ 22-03-01-upload-cos/main.js
@@ -10,6 +10,11 @@ const {
 
 const filePath = p;
 
+if (!filePath) {
+  console.log('请输入文件路径: node main.js -p path_to_file');
+  process.exit(1);
+}
+
 require('dotenv')
   .config({ path: path.resolve('.', '.env') });
 
@@ -25,19 +30,42 @@ const cos = new COS({
 const postfix = filePath.split('.').pop();
 const fileName = `${uuid()}.${postfix}`;
 const fileKey = `${Key}/${fileName}`;
-cos.putObject(
-  {
-    Bucket,
-    Region,
-    Key: fileKey,
-    Body: fs.createReadStream(filePath),
-  },
-  (err, data) => {
-    if (err) {
-      return console.error(err);
-    }
-    console.log(data);
 
-    console.log(`${CName}/${fileKey}`);
+// 小文件上传
+// cos.putObject(
+//   {
+//     Bucket,
+//     Region,
+//     Key: fileKey,
+//     Body: fs.createReadStream(filePath),
+//   },
+//   (err, data) => {
+//     if (err) {
+//       return console.error(err);
+//     }
+//     console.log(data);
+
+//     console.log(`${CName}/${fileKey}`);
+//   },
+// );
+
+// 大文件分片上传
+cos.sliceUploadFile({
+  Bucket, /* 填入您自己的存储桶，必须字段 */
+  Region, /* 存储桶所在地域，例如ap-beijing，必须字段 */
+  Key: fileKey, /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */
+  FilePath: filePath, /* 必须 */
+  SliceSize: 1024 * 1024 * 10, // 大于10M分片上传
+  AsyncLimit: 100,
+  onTaskReady(taskId) { /* 非必须 */
+    console.log(taskId);
   },
-);
+  onHashProgress(progressData) { /* 非必须 */
+    console.log(JSON.stringify(progressData));
+  },
+  onProgress(progressData) { /* 非必须 */
+    console.log(JSON.stringify(progressData));
+  },
+}, (err, data) => {
+  console.log(err || data);
+});
